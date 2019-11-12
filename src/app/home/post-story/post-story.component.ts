e2f29a4318023751610f9bb3048ae3468ceae8fd
Pablo <yaexiste_52@hotmail.com>
2019-11-11 23:51:51 -0300

    Primer enfoque para la UI del Home.

--- src/app/home/post-story/post-story.component.ts
+++ src/app/home/post-story/post-story.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-post-story',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostStoryComponent implements OnInit {
 
-  constructor() { }
+  frmPost = this.fb.group({
+    post: ['']
+  });
+
+  get post(): any {
+    return this.frmPost.get('post');
+  }
+
+  set post(post: any) {
+    this.frmPost.get('post').setValue(post);
+  }
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(event: Event) {
+    console.log('ESCUCHANDO SUBMIT');
+  }
+
+  textareaAutoSize(event: Event) {
+    (event.target as HTMLElement).style.height = '0px';
+    (event.target as HTMLElement).style.height = ((event.target as HTMLElement).scrollHeight) + 'px';
   }
 
 }
--- src/app/home/post-story/post-story.module.ts
+++ src/app/home/post-story/post-story.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { PostStoryRoutingModule } from './post-story-routing.module';
 import { PostStoryComponent } from './post-story.component';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+const MATCOMPONENTS: any[] = [
+  MatCardModule,
+  MatInputModule,
+  MatIconModule,
+  MatButtonModule,
+];
 
 @NgModule({
   exports: [
@@ -14,7 +25,8 @@ import { PostStoryComponent } from './post-story.component';
   ],
   imports: [
     CommonModule,
-    PostStoryRoutingModule
-  ]
+    PostStoryRoutingModule,
+    ReactiveFormsModule
+  ].concat(MATCOMPONENTS)
 })
 export class PostStoryModule { }
